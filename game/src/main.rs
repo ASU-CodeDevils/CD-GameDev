9b5bba7e917173fe51a23dcbc5c6845c39420131
Joshua Walker <walkerje1992@gmail.com>
2024-10-22 17:40:17 -0600

    feat: add load level from LDtk
    add capability to load level from LDtk

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,4 +1,4 @@
-#[allow(dead_code)]
+#![allow(dead_code)]
 mod components;
 mod debug;
 mod movement;
@@ -20,5 +20,21 @@ fn main() {
         //user plugins
         .add_plugins(HealthAndDamagePlugin)
         .add_plugins(DebugPlugin)
+        //Delete me
+        .add_systems(Startup, setup)
+        .insert_resource(LevelSelection::index(0))
         .run();
+}
+
+fn setup(mut command: Commands, asset_server: Res<AssetServer>) {
+    let mut camera = Camera2dBundle::default();
+    camera.projection.scale = 1.0;
+    camera.transform.translation.x += 1280.0 / 4.0;
+    camera.transform.translation.y += 720.0 / 8.0;
+    command.spawn(camera);
+
+    command.spawn(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("TestLevel.ldtk"),
+        ..Default::default()
+    });
 }
