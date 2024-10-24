d53a4a05178bec08dd9db584243d0d89e6704b52
Joshua Walker <walkerje1992@gmail.com>
2024-10-23 21:42:46 -0600

    feat: add movement and collisions WIP
    add movement for player and collisions to prevent going through walls

--- game/src/main.rs
+++ game/src/main.rs
@@ -11,10 +11,45 @@ use debug::debug_plugin::DebugPlugin;
 use movement::plugin::CharacterControllerPlugin;
 use plugins::health_and_damage_plugin::HealthAndDamagePlugin;
 
+use std::collections::HashSet;
+
+const GRID_SIZE: i32 = 24;
+
+#[derive(Default, Component)]
+struct Player;
+
 #[derive(Default, Bundle, LdtkEntity)]
 struct PlayerBundle {
+    player: Player,
     #[sprite_sheet_bundle]
-    sprite_sheet_bundle: LdtkSpriteSheetBundle,
+    sprite_bundle: LdtkSpriteSheetBundle,
+    #[grid_coords]
+    grid_coords: GridCoords,
+}
+
+#[derive(Default, Component)]
+struct Wall;
+
+#[derive(Default, Bundle, LdtkIntCell)]
+struct WallBundle {
+    wall: Wall,
+}
+
+#[derive(Default, Resource)]
+struct LevelWalls {
+    wall_locations: HashSet<GridCoords>,
+    level_width: i32,
+    level_height: i32,
+}
+
+impl LevelWalls {
+    fn in_wall(&self, grid_coords: &GridCoords) -> bool {
+        grid_coords.x < 0
+            || grid_coords.y < 0
+            || grid_coords.x >= self.level_width
+            || grid_coords.y >= self.level_height
+            || self.wall_locations.contains(grid_coords)
+    }
 }
 
 fn main() {
@@ -30,6 +65,16 @@ fn main() {
         .add_systems(Startup, setup)
         .insert_resource(LevelSelection::index(0))
         .register_ldtk_entity::<PlayerBundle>("Player")
+        .add_systems(
+            Update,
+            (
+                move_player_from_input,
+                translate_grid_coords_entities,
+                cache_wall_locations,
+            ),
+        )
+        .register_ldtk_int_cell::<WallBundle>(1)
+        .init_resource::<LevelWalls>()
         .run();
 }
 
@@ -44,6 +89,68 @@ fn setup(mut command: Commands, asset_server: Res<AssetServer>) {
         ldtk_handle: asset_server.load("TestLevel.ldtk"),
         ..Default::default()
     });
+}
 
-    
+fn move_player_from_input(
+    mut players: Query<&mut GridCoords, With<Player>>,
+    input: Res<ButtonInput<KeyCode>>,
+    level_walls: Res<LevelWalls>,
+) {
+    let movement_direction = if input.just_pressed(KeyCode::KeyW) {
+        GridCoords::new(0, 1)
+    } else if input.just_pressed(KeyCode::KeyA) {
+        GridCoords::new(-1, 0)
+    } else if input.just_pressed(KeyCode::KeyS) {
+        GridCoords::new(0, -1)
+    } else if input.just_pressed(KeyCode::KeyD) {
+        GridCoords::new(1, 0)
+    } else {
+        return;
+    };
+
+    for mut player_grid_coords in players.iter_mut() {
+        let destination = *player_grid_coords + movement_direction;
+        if !level_walls.in_wall(&destination) {
+            *player_grid_coords = destination;
+        }
+    }
+}
+
+fn translate_grid_coords_entities(
+    mut grid_coords_entities: Query<(&mut Transform, &GridCoords), Changed<GridCoords>>,
+) {
+    for (mut transform, grid_coords) in grid_coords_entities.iter_mut() {
+        transform.translation =
+            bevy_ecs_ldtk::utils::grid_coords_to_translation(*grid_coords, IVec2::splat(GRID_SIZE))
+                .extend(transform.translation.z);
+    }
+}
+
+fn cache_wall_locations(
+    mut level_walls: ResMut<LevelWalls>,
+    mut level_events: EventReader<LevelEvent>,
+    walls: Query<&GridCoords, With<Wall>>,
+    ldtk_project_entities: Query<&Handle<LdtkProject>>,
+    ldtk_project_assets: Res<Assets<LdtkProject>>,
+) {
+    for level_event in level_events.read() {
+        if let LevelEvent::Spawned(level_iid) = level_event {
+            let ldtk_project = ldtk_project_assets
+                .get(ldtk_project_entities.single())
+                .expect("LdtkProject should be loaded when level is spawned");
+            let level = ldtk_project
+                .get_raw_level_by_iid(level_iid.get())
+                .expect("spawned level should exist in project");
+
+            let wall_locations = walls.iter().copied().collect();
+
+            let new_level_walls = LevelWalls {
+                wall_locations,
+                level_width: level.px_wid / GRID_SIZE,
+                level_height: level.px_hei / GRID_SIZE,
+            };
+
+            *level_walls = new_level_walls;
+        }
+    }
 }
